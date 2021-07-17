bc99afc5c614de27b6867ad053f59757655c671f
Luma <world@luma.email>
2021-07-18 03:14:38 +0900

    chore: deafulting env values to non-null string to start easily

--- src/server/env.ts
+++ src/server/env.ts
@@ -1,5 +1,9 @@
-const API_JWT_SECRET = process.env.API_JWT_SECRET ?? "";
-const API_USER_ID = process.env.API_USER_ID ?? "";
-const API_USER_PASS = process.env.API_USER_PASS ?? "";
+import crypto from "crypto";
+
+// TODO(user): Defaulting to non-null string is only for demonstration to start easily.
+const API_JWT_SECRET =
+  process.env.API_JWT_SECRET ?? crypto.randomBytes(32).toString("hex");
+const API_USER_ID = process.env.API_USER_ID ?? "user";
+const API_USER_PASS = process.env.API_USER_PASS ?? "pass";
 
 export { API_JWT_SECRET, API_USER_ID, API_USER_PASS };
